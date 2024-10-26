880fe119896c73943c52dd8f19c098a6ae74bda5
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-10-26 22:05:17 +0530

    added Oracle contract

---
+++ contracts/src/interfaces/IOracle.ts
@@ -0,0 +1,56 @@
+export class openAIRequest {
+  // "gpt-4-turbo", "gpt-4-turbo-preview" or "gpt-3.5-turbo-1106"
+  model: string;
+  // int -20 - 20, Mapped to float -2.0 - 2.0. If bigger than 20 then null
+  frequencyPenalty: number;
+  // JSON string or empty string
+  logitBias: string;
+  // 0 for null
+  maxTokens: number;
+  // int -20 - 20, Mapped to float -2.0 - 2.0. If bigger than 20 then null
+  presencePenalty: number;
+  // JSON string or empty string
+  responseFormat: string;
+  // 0 for null
+  seed: number;
+  // empty str for null
+  stop: string;
+  // 0-20, > 20 for null
+  temperature: number;
+  // 0-100  percentage, > 100 for null
+  topP: number;
+  // JSON list for tools in OpenAI format, empty for null, names have to match the supported tools
+  tools: string;
+  // "none", "auto" or empty str which defaults to auto on OpenAI side
+  toolChoice: string;
+
+  user: string;
+}
+
+export class openAIResponse {
+  id: string;
+
+  // either content is an empty str or functionName and functionArguments
+  content: string;
+  functionName: string;
+  functionArguments: string;
+
+  created: number;
+  model: string;
+  systemFingerprint: string;
+  // kind of pointless since its always "chat.completion"?
+  object: string;
+
+  completionTokens: number;
+  promptTokens: number;
+  totalTokens: number;
+}
+
+export interface IOracle {
+  createOpenAiLlmCall(promptId: number, request: openAIRequest): number;
+  createFunctionCall(
+    functionCallbackId: number,
+    functionType: string,
+    functionInput: string
+  ): number;
+}
---
+++ contracts/src/oracle/contract.ts
@@ -0,0 +1,160 @@
+import {
+  NearBindgen,
+  near,
+  call,
+  view,
+  initialize,
+  LookupSet,
+  LookupMap,
+  assert,
+  NearPromise,
+} from "near-sdk-js";
+import { AccountId } from "near-sdk-js";
+import { IOracle, openAIRequest, openAIResponse } from "../interfaces/IOracle";
+
+const THIRTY_TGAS = BigInt("30000000000000");
+
+@NearBindgen({ requireInit: true })
+class Oracle implements IOracle {
+  private owner: AccountId;
+  private promptsCount: number;
+  private functionsCount: number;
+  private whitelistedAddresses: LookupSet<AccountId> = new LookupSet<AccountId>(
+    "WA"
+  );
+  private callbackAddresses: LookupMap<AccountId> = new LookupMap<AccountId>(
+    "CA"
+  );
+
+  private promptCallbackIds: LookupMap<number> = new LookupMap<number>("pCI");
+  private isPromptProcessed: LookupMap<boolean> = new LookupMap<boolean>("iPP");
+
+  // @notice Mapping of prompt ID to the OpenAI configuration
+  private openAiConfigurations: LookupMap<openAIRequest> =
+    new LookupMap<openAIRequest>("oAC");
+
+  @initialize({})
+  init() {
+    this.owner = near.signerAccountId();
+    this.promptsCount = 0;
+    this.functionsCount = 0;
+  }
+
+  private onlyOwner(): void {
+    assert(near.predecessorAccountId() == this.owner, "Caller is not owner");
+  }
+
+  private onlyWhitelisted(): void {
+    assert(
+      this.whitelistedAddresses.contains(near.predecessorAccountId()) == true,
+      "Caller is not whitelisted"
+    );
+  }
+
+  private promptAlreadyProcessed(promptID: number): void {
+    assert(
+      this.isPromptProcessed.get(promptID.toString()) == false,
+      "Prompt Already processed"
+    );
+  }
+
+  @call({})
+  addWhitelistedAdress(addressToWhitelist: AccountId): void {
+    this.onlyOwner();
+    this.whitelistedAddresses.set(addressToWhitelist);
+  }
+
+  @call({})
+  createOpenAiLlmCall(
+    promptCallbackID: number,
+    request: openAIRequest
+  ): number {
+    const promptId = this.promptsCount;
+    this.callbackAddresses.set(
+      promptId.toString(),
+      near.predecessorAccountId()
+    );
+
+    this.promptCallbackIds.set(promptId.toString(), promptCallbackID);
+    this.isPromptProcessed.set(promptId.toString(), false);
+    this.openAiConfigurations.set(promptId.toString(), request);
+    this.promptsCount += 1;
+
+    return promptId;
+  }
+
+  @call({})
+  addOpenAiResponse(
+    promptId: number,
+    promptCallbackID: number,
+    response: openAIResponse,
+    error: string
+  ): void {
+    this.onlyWhitelisted();
+    this.promptAlreadyProcessed(promptId);
+    this.isPromptProcessed.set(promptId.toString(), true);
+
+    const callbackAddress = this.callbackAddresses.get(promptId.toString());
+    assert(callbackAddress != null, "Callback address not found");
+    const promise = NearPromise.new(callbackAddress).functionCall(
+      "onOracleOpenAiLlmResponse",
+      JSON.stringify({
+        promptCallbackId: promptCallbackID,
+        response: response,
+        errorMessage: error,
+      }),
+      BigInt(0),
+      THIRTY_TGAS
+    );
+  }
+
+  @view({})
+  getMessages(promptId: number, promptCallbackID: number): any {
+    const callbackAddress = this.callbackAddresses.get(promptId.toString());
+    assert(callbackAddress != null, "Callback address not found");
+    const promise = NearPromise.new(callbackAddress).functionCall(
+      "getMessageHistoryContents",
+      JSON.stringify({}),
+      BigInt(0),
+      THIRTY_TGAS
+    );
+
+    return promise;
+  }
+
+  @view({})
+  getRoles(promptId: number, promptCallbackID: number): any {
+    const callbackAddress = this.callbackAddresses.get(promptId.toString());
+    assert(callbackAddress != null, "Callback address not found");
+    const promise = NearPromise.new(callbackAddress).functionCall(
+      "getMessageHistoryRoles",
+      JSON.stringify({}),
+      BigInt(0),
+      THIRTY_TGAS
+    );
+
+    return promise;
+  }
+
+  @view({})
+  getMessagesAndRoles(promptId: number, promptCallbackId: number): any {
+    const callbackAddress = this.callbackAddresses.get(promptId.toString());
+    assert(callbackAddress != null, "Callback address not found");
+    const promise = NearPromise.new(callbackAddress).functionCall(
+      "getMessageHistory",
+      JSON.stringify({}),
+      BigInt(0),
+      THIRTY_TGAS
+    );
+
+    return promise;
+  }
+
+  createFunctionCall(
+    functionCallbackId: number,
+    functionType: string,
+    functionInput: string
+  ): number {
+    return 1;
+  }
+}
